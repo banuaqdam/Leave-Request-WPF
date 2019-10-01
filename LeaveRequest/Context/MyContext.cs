4e3af8028cbe965ec6fa1eb3e0197d81db3bc190
Banu Aqdam <banuaqdam@gmail.com>
2019-10-01 16:25:41 +0700

    Context/MyContext.cs

--- LeaveRequest/Context/MyContext.cs
+++ LeaveRequest/Context/MyContext.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LeaveRequest.Models;
+using System.Data.Entity;
 
 namespace LeaveRequest.Context
 {
-    class MyContext
+    class MyContext : DbContext
     {
+        public MyContext() : base("DbConnectionString")
+        {
+
+        }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Division> Divisions { get; set; }
+        public DbSet<Holiday> Holidays { get; set; }
+        public DbSet<Leave_Data> Leave_Datas { get; set; }
+        public DbSet<Leave_Status> Leave_Status { get; set; }
+        public DbSet<Roles> Roless { get; set; }
+        public DbSet<TR_Leave> TR_Leaves { get; set; }
+        public DbSet<Type_Leave> Type_Leaves { get; set; }
     }
 }
