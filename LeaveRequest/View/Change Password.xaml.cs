d6a285e808eedb4820cbbb5cf7b4a086ea750f05
Sulistyo Adi Nugroho <sulistyoadi96@gmail.com>
2019-10-03 17:34:34 +0700

    UI Login Password

---
+++ LeaveRequest/View/Change Password.xaml.cs
@@ -0,0 +1,55 @@
+﻿using LeaveRequest.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LeaveRequest.View
+{
+    /// <summary>
+    /// Interaction logic for Change_Password.xaml
+    /// </summary>
+    public partial class Change_Password : Window
+    {
+        public string G_NIK;
+        public Change_Password(string NIK)
+        {
+            InitializeComponent();
+            setNIK(NIK);
+            setName(NIK);
+        }
+        public void setNIK(string NIK)
+        {
+            G_NIK = NIK;
+        }
+        public void setName(string NIK)
+        {
+            MyContext _context = new MyContext();
+            var get = _context.Employees.Find(NIK);
+            string FName = get.First_Name;
+            string LName = get.Last_Name;
+        }
+
+        private void Btn_Forgot_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Btn_Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+
+            Home home = new Home(G_NIK);
+            home.Show();
+            this.Close();
+        }
+    }
+}
